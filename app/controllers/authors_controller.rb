8d7e5db1d9ec1d41d5f7493ad6b7a6b6501c9178
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-23 20:59:31 -0400

    commit tree b3ee10d5a22eff73204e36225d558077a85c01d7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,22 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    # @author = Author.create!(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      # If--and only if--the post is valid, do what we usually do.
+      @author.save
+      # This returns a status_code of 302, which instructs the browser to
+      # perform a NEW REQUEST! (AKA: throw @post away and let the show action
+      # worry about re-reading it from the database)
+      redirect_to author_path(@author)
+    else
+      # If the post is invalid, hold on to @post, because it is now full of
+      # useful error messages, and re-render the :new page, which knows how
+      # to display them alongside the user's entries.
+      render :new
+    end
+    # redirect_to author_path(@author)
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,15 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    # @post.update(post_params)
+    # redirect_to post_path(@post)
+    @posty = Post.new(post_params)
+    if @posty.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
